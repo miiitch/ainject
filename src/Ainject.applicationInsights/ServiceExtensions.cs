97555825b8ef2746f2d643c107e1a16fd571ba52
Michel Perfetti <miiitch@gmail.com>
2019-03-15 13:33:00 +0100

    Add ServiceCollection registration

---
+++ src/Ainject.applicationInsights/ServiceExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Ainject.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ainject.ApplicationInsights
+{
+    // ReSharper disable once InconsistentNaming
+    public static class ServiceCollectionExtensions {
+
+        public static IServiceCollection AddAInject(this IServiceCollection services)
+        {
+            return services.AddScoped<ITelemetry, ApplicationInsightTelemetry>();
+        }
+    }
+}
