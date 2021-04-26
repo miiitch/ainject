240bdf54f693ee74ae3636e4a0d468fc5b959ca6
Michel Perfetti <miiitch@gmail.com>
2021-04-26 14:33:39 +0200

    Can use a specific telemetry for AInject in asp.net core

--- src/Ainject.applicationInsights/ServiceExtensions.cs
+++ src/Ainject.applicationInsights/ServiceExtensions.cs
@@ -5,20 +5,44 @@ using Microsoft.Extensions.DependencyInjection;
 namespace Ainject.ApplicationInsights
 {
     // ReSharper disable once InconsistentNaming
-    public static class ServiceCollectionExtensions {
-
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Configures AINject with Application Insights
+        /// </summary>
+        /// <param name="services"></param>
+        /// <returns></returns>
         public static IServiceCollection AddAInject(this IServiceCollection services)
         {
             return services.AddSingleton<ITelemetry, ApplicationInsightTelemetry>();
         }
-
+        
+        /// <summary>
+        /// Configures AInject with Application Insights and a custom telemetry data
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="defaultData"></param>
+        /// <returns></returns>
         public static IServiceCollection AddAInject(this IServiceCollection services, TelemetryData defaultData)
         {
+            
             return services.AddSingleton<ITelemetry>(svcs =>
             {
                 var telemetryClient = svcs.GetService<TelemetryClient>();
+
                 return new ApplicationInsightTelemetry(telemetryClient, defaultData);
             });
+        }
+        
+        /// <summary>
+        /// Configures AInject with a custom telemetry
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="telemetry"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddAInject(this IServiceCollection services, ITelemetry telemetry)
+        {
+            return services.AddSingleton<ITelemetry>(telemetry);
         }
     }
 }
